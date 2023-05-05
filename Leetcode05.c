039d9ebbfd0931ee5292311d0652442c8f3c7fea
林政浩 <1392872316@qq.com>
2023-05-06 00:30:46 +0800

    0506

--- Leetcode05.c
+++ Leetcode05.c
@@ -1,31 +1,32 @@
 char* longestPalindrome(char* s) {
-    int start, i, max, index;
+    int start, i, max, index, temp_max;
     index = max = start = 0;
-    int left = 0, right = strlen(s) - 1;
-    for (i = 0; i < strlen(s) / 2; i++) {
-        if (s[left] != s[right])
-            break;
-        left++, right--;
-        if (left == strlen(s) / 2 - 1)
-            max = strlen(s);
-    }
-
+    int left, right;
 
     for (i = 0; i < strlen(s); i++) {
         if (i < strlen(s) - 1 && s[i] == s[i + 1]) {
             index = i;
-            while (s[index] == s[index + 1] && index < strlen(s) - 1) {
+            while (index < strlen(s) - 1 && s[index] == s[index + 1]) {
                 index++;
             }
             if (index - i + 1 > max) {
                 max = index - i + 1;
                 start = i;
             }
-            continue;
+            left = i - 1, right = i + 2;
+            temp_max = 2;
+            while (left >= 0 && s[left] == s[right]) {
+                left--, right++;
+                temp_max += 2;
+            }
+            if (temp_max > max) {
+                max = temp_max;
+                start = left + 1;
+            }
         }
-        else if (i > 0 && i < strlen(s) - 1 && s[i - 1] == s[i + 1]) {
+        if (i > 0 && i < strlen(s) - 1 && s[i - 1] == s[i + 1]) {
             left = i - 1, right = i + 1;
-            int temp_max = 1;
+            temp_max = 1;
             while (left >= 0 && right < strlen(s) && s[left] == s[right]) {
                 temp_max += 2;
                 left--, right++;
@@ -34,7 +35,6 @@ char* longestPalindrome(char* s) {
                 max = temp_max;
                 start = left + 1;
             }
-            continue;
         }
     }
     if (max == 0) {

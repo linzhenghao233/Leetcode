8cf3996dd464f33f113f4cb0d1aa36dc56c4bb5e
林政浩 <1392872316@qq.com>
2023-05-23 01:21:10 +0800

    0523

--- Leetcode70.c
+++ Leetcode70.c
@@ -1,17 +1,37 @@
+//int climbStairs(int n) {
+//    int sum = 1;
+//    int two = 1;
+//
+//    while (2 * two <= n) {
+//        unsigned long long dividend = 1;//分子
+//        unsigned long long divisor = 1;//分母
+//
+//        for (int i = 0; i < two; i++) {
+//            dividend *= (n - two - i);
+//            divisor *= (two - i);
+//        }
+//        sum += dividend / divisor;
+//        two++;
+//    }
+//
+//    return sum;
+//}
+//阶乘加循环会溢出，用动态规划做
 int climbStairs(int n) {
-    int sum = 1 + n - 1;
-    int x = n - 2 - 1;
-    int two = 2;
-    int amount = n - 2;
+    int* function;
+    int answer;
 
-    while (two <= amount) {
-        x = n - two - 1;
-        while (x >= 1) {
-            sum += x;
-            x--;
-        }
-        two++;
-        amount--;
-    }
-    return sum;
+    function = (int*)malloc(46 * sizeof(int));
+    function[0] = 1;
+    function[1] = 1;
+
+    if (n == 1)
+        return 1;
+
+    for (int i = 2; i <= n; i++)
+        function[i] = function[i - 1] + function[i - 2];
+    answer = function[n - 1] + function[n - 2];
+
+
+    return answer;
 }

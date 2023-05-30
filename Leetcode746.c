5b1efac1259b4546ba93383d6e8dd59c9867cf8f
林政浩 <1392872316@qq.com>
2023-05-30 23:26:29 +0800

    0530

--- Leetcode746.c
+++ Leetcode746.c
@@ -1,8 +1,8 @@
 int minCostClimbingStairs(int* cost, int costSize) {
     int* money;
     money = (int*)malloc((costSize + 1) * sizeof(int));
-    money[0] = cost[0];
-    money[1] = cost[1];
+    money[0] = 0;
+    money[1] = 0;
 
     for (int i = 2; i <= costSize; i++) {
         if (money[i - 1] + cost[i - 1] <= money[i - 2] + cost[i - 2]) {

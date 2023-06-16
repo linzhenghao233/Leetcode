9d35bc8f1b39fe576edcc3d06ed7753f2fe9eee9
linzhenghao233 <1392872316@qq.com>
2023-06-16 23:49:58 +0800

    0616

---
+++ Leetcode62.c
@@ -0,0 +1,15 @@
+int uniquePaths(int m, int n) {
+    int arr[101][101];
+
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            if (i == 0 || j == 0) {
+                arr[i][j] = 1;
+                continue;
+            }
+            arr[i][j] = arr[i - 1][j] + arr[i][j - 1];
+        }
+    }
+
+    return arr[m - 1][n - 1];
+}

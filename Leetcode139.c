9e0ffd8b6e077b256595c26d785052ac0909284c
linzhenghao233 <1392872316@qq.com>
2023-07-07 23:05:38 +0800

    0707

--- Leetcode139.c
+++ Leetcode139.c
@@ -1,15 +1,17 @@
 bool wordBreak(char* s, char** wordDict, int wordDictSize) {
     int s_len = strlen(s);
-    int k = 0;
+    int k = 0, len = 1;
 
     for (int i = 0; i < strlen(s); i++) {
         for (int j = 0; j < wordDictSize; j++) {
-            if (!strncmp(s + k, wordDict[j], strlen(wordDict[j]))) {
-                k = strlen(wordDict[j]);
+            if (len == strlen(wordDict[j]) && !strncmp(s + k, wordDict[j], strlen(wordDict[j]))) {
+                k += strlen(wordDict[j]);
                 s_len -= strlen(wordDict[j]);
+                len = 0;
                 break;
             }
         }
+        len++;
     }
     if (s_len == 0)
         return true;

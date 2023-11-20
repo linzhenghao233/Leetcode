b4b6faf4d5be78cc9f6a5fa719176217d7408e22
linzhenghao233 <1392872316@qq.com>
2023-11-20 22:55:37 +0800

    1120

--- Leetcode06.c
+++ Leetcode06.c
@@ -44,26 +44,20 @@ char* convert(char* s, int numRows) {
 char* convert(char* s, int numRows) {
     int len = strlen(s);
     int loop = 2 * numRows - 2;
-    int num, row, mul;
-    num = row = 0;
-    mul = 1;
+    int num = 0;
     char* result = (char*)malloc((len + 1) * sizeof(char));
 
-    while (row < numRows) {
-        result[num] = s[num];
-        num++;
-        while (row + mul * loop < len) {
-            int temp = row + mul * loop - 2 * row;
-            if (row != 0 && row != numRows - 1 && temp < len) {
-                result[num] = s[temp];
-                num++;
+    if (numRows == 1 || numRows >= len)
+        return s;
+
+    for (int i = 0; i < numRows; i++) {
+        for (int j = i; j < len; j += loop) {
+            result[num++] = s[j];
+            int temp = j + loop - 2 * i;
+            if (i != 0 && i != numRows - 1 && temp < len) {
+                result[num++] = s[temp];
             }
-            result[num] = s[row + mul * loop];
-            num++;
-            mul++;
         }
-        mul = 1;
-        row++;
     }
     result[num] = '\0';
 

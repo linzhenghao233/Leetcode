f469eee67c3fd22a36e7274532451b3af1fe2563
linzhenghao233 <1392872316@qq.com>
2023-11-23 23:21:05 +0800

    1123

--- Leetcode10.c
+++ Leetcode10.c
@@ -39,6 +39,27 @@ bool isMatch(char* s, char* p) {
     else
         return false;
 }
-"aaa"
-"a*a"
-target : true
+
+
+//以上的代码未完成，下面的代码用递归，时间和空间都用太多
+bool isMatch(char* s, char* p) {
+    int i, j, s_len, p_len;
+    s_len = strlen(s);
+    p_len = strlen(p);
+
+    for (i = 0, j = 0; i < s_len || j < p_len; i++, j++) {
+        if (j + 1 < p_len && p[j + 1] == '*') {
+            char temp = p[j];
+            j++;
+            while (i < s_len && (s[i] == temp || temp == '.')) {
+                if (isMatch(s + i, p + j + 1))
+                    return true;
+                i++;
+            }
+            return isMatch(s + i, p + j + 1);
+        }
+        if (i >= s_len || (s[i] != p[j] && p[j] != '.'))
+            return false;
+    }
+    return true;
+}

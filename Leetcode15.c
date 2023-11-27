1d373a43842160c11b793a45b61b027040e410de
linzhenghao233 <1392872316@qq.com>
2023-11-28 01:28:35 +0800

    1128

--- Leetcode15.c
+++ Leetcode15.c
@@ -1,49 +1,56 @@
+/**
+ * Return an array of arrays of size *returnSize.
+ * The sizes of the arrays are returned as *returnColumnSizes array.
+ * Note: Both returned array and *columnSizes array must be malloced, assume caller calls free().
+ */
 int** threeSum(int* nums, int numsSize, int* returnSize, int** returnColumnSizes) {
-    int row = 0;
-    int** arr = (int**)malloc(1000 * sizeof(int*));
+    int left, middle, right, row = 0, target;
+    int** arr = (int**)malloc(numsSize * numsSize / 6 * sizeof(int*));
 
-    // Sort the array first to apply the two-pointer approach
-    qsort(nums, numsSize, sizeof(int), compareFunc);
-
-    for (int left = 0; left < numsSize - 2; left++) {
-        if (left > 0 && nums[left] == nums[left - 1])
-            continue;
-
-        int mid = left + 1;
-        int right = numsSize - 1;
-
-        while (mid < right) {
-            int sum = nums[left] + nums[mid] + nums[right];
-            if (sum == 0) {
-                arr[row] = (int*)malloc(3 * sizeof(int));
-                arr[row][0] = nums[left];
-                arr[row][1] = nums[mid];
-                arr[row][2] = nums[right];
-                row++;
-
-                // Skip duplicates
-                while (mid < right && nums[mid] == nums[mid + 1])
-                    mid++;
-                while (mid < right && nums[right] == nums[right - 1])
-                    right--;
-
-                mid++;
-                right--;
-            }
-            else if (sum < 0) {
-                mid++;
-            }
-            else {
-                right--;
+    for (int i = 0; i < numsSize - 1; i++) {
+        for (int j = 0; j < numsSize - 1; j++) {
+            if (nums[j] > nums[j + 1]) {
+                int temp = nums[j];
+                nums[j] = nums[j + 1];
+                nums[j + 1] = temp;
             }
         }
     }
 
-    *returnSize = row;
-    *returnColumnSizes = (int*)malloc(row * sizeof(int));
-    for (int i = 0; i < row; i++) {
-        (*returnColumnSizes)[i] = 3;
-    }
+    for (left = 0; left + 2 < numsSize; left++) {
+        if (left > 0 && nums[left] == nums[left - 1])
+            continue;
 
+        middle = left + 1;
+        right = numsSize - 1;
+        while (middle < right) {
+            target = nums[left] + nums[middle] + nums[right];
+            if (target == 0) {
+                arr[row] = (int*)malloc(3 * sizeof(int));
+                arr[row][0] = nums[left];
+                arr[row][1] = nums[middle];
+                arr[row][2] = nums[right];
+                row++;
+
+                while (middle < right && nums[middle + 1] == nums[middle])
+                    middle++;
+                while (middle < right && nums[right - 1] == nums[right])
+                    right--;
+                middle++;
+                right--;
+            }
+            else if (target > 0)
+                right--;
+            else
+                middle++;
+        }
+        *returnSize = row;
+        *returnColumnSizes = (int*)malloc(row * sizeof(int));
+        for (int i = 0; i < row; i++) {
+            (*returnColumnSizes)[i] = 3;
+        }
+    }
     return arr;
 }
+
+//ÔËÐÐÄÚ´æ400m£¨- _ -£©

737ababb1893be8dc32d0de7432497b38ab786a1
linzhenghao233 <1392872316@qq.com>
2023-12-07 22:59:20 +0800

    1207

--- Leetcode18.c
+++ Leetcode18.c
@@ -9,47 +9,59 @@ int cmp(const void* a, const void* b) {
 
 int** fourSum(int* nums, int numsSize, int target, int* returnSize, int** returnColumnSizes) {
     qsort(nums, numsSize, sizeof(int), cmp);
-    int head = 0, tail = numsSize - 1, left, right, row = 0;
+    int left, right, row = 0, i, j;
     int** result = (int**)malloc(numsSize * 4 * sizeof(int*));
     *returnColumnSizes = (int*)malloc(numsSize * 4 * sizeof(int));
 
-    while (head < tail) {
-        left = head + 1;
-        right = tail - 1;
-        while (left < tail && right > head && left < right) {
-            int total = nums[head] + nums[left] + nums[right] + nums[tail];
-            if (total < target) {
-                while (left + 1 < right && nums[left] == nums[left + 1])
+    if (nums[0] > 0 && nums[0] > target) {
+        *returnSize = 0;
+        *returnColumnSizes = NULL;
+        return NULL;
+    }
+    if (nums[numsSize - 1] < 0 && nums[numsSize - 1] < target) {
+        *returnSize = 0;
+        *returnColumnSizes = NULL;
+        return NULL;
+    }
+
+    for (i = 0; i < numsSize - 3; i++) {
+        if (i > 0 && nums[i] == nums[i - 1])
+            continue;
+        for (j = i + 1; j < numsSize - 2; j++) {
+            if (j > i + 1 && nums[j] == nums[j - 1])
+                continue;
+            left = j + 1;
+            right = numsSize - 1;
+
+            while (left < right) {
+                long long total = (long long)nums[i] + nums[j] + nums[left] + nums[right];
+                if (total < target) {
+                    while (left + 1 < right && nums[left] == nums[left + 1])
+                        left++;
                     left++;
-                left++;
-            }
-            else if (total > target) {
-                while (right - 1 < right && nums[right] == nums[right - 1])
+                }
+                else if (total > target) {
+                    while (right - 1 < right && nums[right] == nums[right - 1])
+                        right--;
                     right--;
-                right--;
-            }
-            else {
-                result[row] = (int*)malloc(4 * sizeof(int));
-                result[row][0] = nums[head];
-                result[row][1] = nums[left];
-                result[row][2] = nums[right];
-                result[row][3] = nums[tail];
-                (*returnColumnSizes)[row] = 4;
-                row++;
-                left++;
-                right--;
-                while (left < right && nums[left] == nums[left - 1])
+                }
+                else {
+                    result[row] = (int*)malloc(4 * sizeof(int));
+                    result[row][0] = nums[i];
+                    result[row][1] = nums[j];
+                    result[row][2] = nums[left];
+                    result[row][3] = nums[right];
+                    (*returnColumnSizes)[row] = 4;
+                    row++;
                     left++;
-                while (left < right && nums[right] == nums[right + 1])
                     right--;
+                    while (left < right && nums[left] == nums[left - 1])
+                        left++;
+                    while (left < right && nums[right] == nums[right + 1])
+                        right--;
+                }
             }
         }
-        head++;
-        tail--;
-        while (head < tail && nums[head] == nums[head - 1])
-            head++;
-        while (head < tail && nums[tail] == nums[tail + 1])
-            tail--;
     }
     *returnSize = row;
 

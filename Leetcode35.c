494a77df0765773c800664d001b668099436a9ce
linzhenghao233 <1392872316@qq.com>
2023-12-16 22:22:13 +0800

    1216

---
+++ Leetcode35.c
@@ -0,0 +1,17 @@
+int searchInsert(int* nums, int numsSize, int target) {
+    int left, right, middle;
+    left = 0;
+    right = numsSize - 1;
+
+    while (left <= right) {
+        middle = left + (right - left) / 2;
+
+        if (nums[middle] == target)
+            return middle;
+        else if (nums[middle] > target)
+            right = middle - 1;
+        else
+            left = middle + 1;
+    }
+    return left;
+}

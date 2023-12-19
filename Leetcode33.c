3f11822de7cd090ab0ea9957ce07c457d11597de
linzhenghao233 <1392872316@qq.com>
2023-12-19 21:52:44 +0800

    1219

--- Leetcode33.c
+++ Leetcode33.c
@@ -1,3 +1,17 @@
 int search(int* nums, int numsSize, int target) {
-    int res = -1;
+    int res, pos, left, right;
+    res = -1;
+    pos = numsSize / 2;
+    left = 0;
+    right = numsSize - 1;
+
+    while (left < right) {
+        if (nums[left] == target)
+            return left;
+        else if (nums[right] == target)
+            return right;
+        if (nums[pos] > nums[left]) {
+            if (target >= nums[left] && target <= nums[right])
+        }
+    }
 }
--- Leetcode58.c
+++ Leetcode58.c
@@ -1,6 +1,5 @@
 int lengthOfLastWord(char* s) {
     int result, n;
-    char ch;
     result = 0;
     n = strlen(s) - 1;
 

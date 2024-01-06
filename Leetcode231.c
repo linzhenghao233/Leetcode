46b5561f893a6a5953296b2751feb1c60572e372
linzhenghao233 <1392872316@qq.com>
2024-01-06 19:13:35 +0800

    0106

--- Leetcode231.c
+++ Leetcode231.c
@@ -1,6 +1,9 @@
 bool isPowerOfTwo(int n) {
-    if (n % 2 == 0 || n == 1)
-        return true;
-    else
+    if (n <= 0)
         return false;
+
+    int judge = n & (n - 1);
+    if (judge == 0)
+        return true;
+    return false;
 }

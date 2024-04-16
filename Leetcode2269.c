a10628b21b6c6c63b274f29010d4ece61d2f6b8b
linzhenghao233 <1392872316@qq.com>
2024-04-16 19:02:11 +0800

    0416

--- Leetcode2269.c
+++ Leetcode2269.c
@@ -1,10 +1,22 @@
 int divisorSubstrings(int num, int k) {
-    int count = 0;
+    int i = 0;
     int temp = num;
+    int result = 0;
+    int count = 0;
 
-    while (temp > 0) {
-        count++;
+    while (temp) {
         temp /= 10;
+        count++;
     }
+
     temp = num;
+    while (temp && count >= k) {
+        int window = temp % (int)pow(10, k);
+
+        if (window != 0 && num % window == 0)
+            result++;
+        temp /= 10;
+        count--;
+    }
+    return result;
 }

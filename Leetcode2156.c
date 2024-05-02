ea6f94fb28bef40d0031831ace8d045edd387da3
linzhenghao233 <1392872316@qq.com>
2024-05-02 18:57:37 +0800

    0502

--- Leetcode2156.c
+++ Leetcode2156.c
@@ -1,13 +1,23 @@
-//用快速幂改进（还没学会
+long long fast_pow_mod(long long power, int exponent, int modulo) {
+    long long result = 1;
+    while (exponent > 0) {
+        if (exponent % 2 == 1)
+            result = (result * power) % modulo;
+        power = (power * power) % modulo;
+        exponent >>= 1;
+    }
+    return result;
+}
+
 char* subStrHash(char* s, int power, int modulo, int k, int hashValue) {
     char* result = (char*)malloc((k + 1) * sizeof(char));
     int left = 0;
     long long window = 0;
     int count = 0;
-    int last = pow(power, k - 1);
+    long long last = fast_pow_mod(power, k - 1, modulo);
 
     for (int i = 0; i < k; i++) {
-        window += ((int)s[i] - 'a' + 1) * pow(power, count);
+        window = window + ((int)s[i] - 'a' + 1) * fast_pow_mod(power, count, modulo);
         count++;
     }
     if (window % modulo == hashValue) {
@@ -19,7 +29,7 @@ char* subStrHash(char* s, int power, int modulo, int k, int hashValue) {
 
     for (int i = k; i < strlen(s); i++) {
         window -= (int)s[i - k] - 'a' + 1;
-        window /= power;
+        window /= power;    //此处出现了问题，但是没想出办法
         window += ((int)s[i] - 'a' + 1) * last;
         if (window % modulo == hashValue) {
             left = i - k + 1;

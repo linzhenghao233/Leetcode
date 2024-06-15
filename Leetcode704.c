f9d45c2a5282f9bd74c95a1959a07ce19585cbfa
linzhenghao233 <1392872316@qq.com>
2024-06-15 19:22:35 +0800

    0615

---
+++ Leetcode704.c
@@ -0,0 +1,15 @@
+int search(int* nums, int numsSize, int target) {
+    int left = 0, mid, right = numsSize - 1;
+
+    while (left <= right) {
+        mid = left + (right - left) / 2;
+        if (nums[mid] == target)
+            return mid;
+        else if (nums[mid] > target)
+            right = mid - 1;
+        else
+            left = mid + 1;
+    }
+
+    return -1;
+}

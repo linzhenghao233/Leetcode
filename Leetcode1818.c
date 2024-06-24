05fe78d3132a5b9ab58cb043cf376ab47eb624bd
linzhenghao233 <1392872316@qq.com>
2024-06-24 22:34:53 +0800

    0624

--- Leetcode1818.c
+++ Leetcode1818.c
@@ -1,10 +1,10 @@
-//没做出来
+//没做出来。第二次也没做出来。
 int cmp(const void* a, const void* b) {
     return *(int*)a - *(int*)b;
 }
 
 int minAbsoluteSumDiff(int* nums1, int nums1Size, int* nums2, int nums2Size) {
-    int max = 0;
+    int max = INT_MIN;
     long result = 0;
     int target;
     int left = 0, right = nums1Size - 1;
@@ -13,24 +13,28 @@ int minAbsoluteSumDiff(int* nums1, int nums1Size, int* nums2, int nums2Size) {
     for (int i = 0; i < nums1Size; i++) {
         int temp = abs(nums1[i] - nums2[i]);
 
+        result += temp;
         if (temp > max) {
-            result = (result + max) % MOD;
             max = temp;
             target = nums2[i];
         }
-        else
-            result = (result + temp) % MOD;
     }
 
     qsort(nums1, nums1Size, sizeof(int), cmp);
     while (left <= right) {
         int mid = left + (right - left) / 2;
 
-        if (nums1[mid] > target)
+        if (nums1[mid] >= target)
             right = mid - 1;
         else
             left = mid + 1;
     }
 
-    return abs(result + target - nums1[left]) % MOD;
+    left = fmax(left, 0);
+    int new = abs(nums1[left] - target);
+    if (left > 0)
+        new = fmin(new, abs(nums1[left - 1] - target));
+    result = (result - max + new) % MOD;
+
+    return (int)result;
 }

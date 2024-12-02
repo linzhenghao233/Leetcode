a717b4e0a5060398b9e1e38db1cab3afba715659
linzhenghao233 <1392872316@qq.com>
2024-12-02 16:16:50 +0800

    1202

---
+++ Leetcode201.c
@@ -0,0 +1,11 @@
+int rangeBitwiseAnd(int left, int right) {
+    int i = 0;
+
+    while (left != right) {
+        left >>= 1;
+        right >>= 1;
+        i += 1;
+    }
+
+    return left << i;
+}

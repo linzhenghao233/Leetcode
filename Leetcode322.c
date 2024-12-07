f34d526b929b9dc2448c8795ffa5c0d26ab735b5
linzhenghao233 <1392872316@qq.com>
2024-12-07 18:52:51 +0800

    1207

--- Leetcode322.c
+++ Leetcode322.c
@@ -1,22 +1,21 @@
-int cmp(const void* a, const void* b) {
-    return*(int*)a - *(int*)b;
-}
-
 int coinChange(int* coins, int coinsSize, int amount) {
-    int result;
-    qsort(coins, coinsSize, sizeof(int), cmp);
+    int result = -1;
+    int* dp = (int*)malloc((amount + 1) * sizeof(int));
+    int max = amount + 1;
+    for (int i = 1; i <= amount; i++)
+        dp[i] = max;
+    dp[0] = 0;
 
-    for (int i = coinsSize - 1; i >= 0; i--) {
-        int temp = amount;
-        result = 0;
-        for (int j = i; j >= 0; j--) {
-            if (temp >= coins[j]) {
-                result += temp / coins[j];
-                temp %= coins[j];
-            }
-            if (temp == 0)
-                return result;
+    for (int i = 1; i <= coinsSize; i++) {
+        for (int j = 1; j <= amount; j++) {
+            if (coins[i - 1] > j)
+                dp[j] = dp[j];
+            else
+                dp[j] = fmin(dp[j], dp[j - coins[i - 1]] + 1);
         }
     }
-    return -1;
-}//贪心算法不行，需要动态规划
+    result = dp[amount] != max ? dp[amount] : -1;
+    free(dp);
+
+    return result;
+}
